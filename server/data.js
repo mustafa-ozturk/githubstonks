8302baac66f2480bab9ae304db9616443de0192b
Mustafa Ozturk <mozturkpro@gmail.com>
2021-05-24 17:04:42 -0400

    initial hardcoded data

---
+++ server/data.js
@@ -0,0 +1,23 @@
+const Data = {
+    react: {
+        stars: 169000,
+        forks: 34000,
+        commits: 14173,
+        closedIssues: 9837,
+        contributors: 1542,
+    },
+    angular: {
+        stars: 73400,
+        forks: 19300,
+        commits: 21010,
+        closedIssues: 20394,
+        contributors: 1413,
+    },
+    vue: {
+        stars: 184000,
+        forks: 29100,
+        commits: 3175,
+        closedIssues: 9117,
+        contributors: 399,
+    },
+};
