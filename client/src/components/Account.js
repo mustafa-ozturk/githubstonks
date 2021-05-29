2e58777abe3b1179dd99a7cb6a9cc3c431ec3caa
Mustafa Ozturk <mozturkpro@gmail.com>
2021-05-29 09:18:24 -0400

    border radius for account box

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -41,6 +41,7 @@ const Wrapper = styled.div`
     padding: 25px 40px 20px 40px;
     & > div {
         padding: 1rem;
+        border-radius: 8px;
         box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.14),
             0px 2px 1px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);
     }
