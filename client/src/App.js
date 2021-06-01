1bc8232bc4f3f941d3e8caefdd8f6311d3a5d30f
Mustafa Ozturk <mozturkpro@gmail.com>
2021-06-01 12:01:12 -0400

    remove stonkContent

--- client/src/App.js
+++ client/src/App.js
@@ -1,123 +1,25 @@
-import React, { useReducer, useContext } from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import CardContent from "./components/CardContent";
-import StonkContent from "./components/StonkContent";
 import Account from "./components/Account";
+import Stonks from "./components/Stonks";
 import { StonkContext } from "./context/StonkContext";
-
-const initialUserStatsState = {
-    startingBalance: 1000,
-    buysAndSells: [],
-};
-
-function userStatsReducer(state, action) {
-    console.log(action);
-    switch (action.type) {
-        case "PUSH-TO-BUYS-AND-SELLS":
-            return {
-                ...state,
-                buysAndSells: [...state.buysAndSells, action.payload],
-            };
-        default:
-            throw new Error();
-    }
-}
+import { GuestUserContext } from "./context/GuestUserContext";
 
 const App = () => {
     const stonkData = useContext(StonkContext);
-    const [userStats, userStatsDispatch] = useReducer(
-        userStatsReducer,
-        initialUserStatsState
-    );
-
-    console.log(userStats.buysAndSells);
-
-    const getPortfolioValue = () => {
-        // find what stocks we own and how many
-        let stonksOwned = {
-            React: 0,
-            Angular: 0,
-            Vue: 0,
-        };
-        userStats.buysAndSells.forEach((elem) => {
-            if (elem.type === "BUY") {
-                stonksOwned[elem.stockName] += parseInt(elem.quantity);
-            } else {
-                stonksOwned[elem.stockName] -= parseInt(elem.quantity);
-            }
-        });
-        // stock we own * its up to date price
-        let portfolioValue = 0;
-        stonkData.forEach((elem) => {
-            portfolioValue += stonksOwned[elem.name] * elem.price;
-        });
-        return portfolioValue;
-    };
-
-    const getBalance = () => {
-        let total = 0;
-        userStats.buysAndSells.forEach((elem) => {
-            if (elem.type === "BUY") {
-                total += parseFloat(elem.purchaseCost);
-            } else {
-                total -= parseFloat(elem.purchaseCost);
-            }
-        });
-        return userStats.startingBalance - total;
-    };
-
-    const getNetWorth = () => {
-        return getPortfolioValue() + getBalance();
-    };
-
-    const getProfitLoss = () => {
-        let stonksOwned = {
-            React: 0,
-            Angular: 0,
-            Vue: 0,
-        };
-        let totalCostAtPurchase = 0;
-        userStats.buysAndSells.forEach((elem) => {
-            if (elem.type === "BUY") {
-                stonksOwned[elem.stockName] += parseInt(elem.quantity);
-                totalCostAtPurchase += parseFloat(elem.purchaseCost);
-            } else {
-                stonksOwned[elem.stockName] -= parseInt(elem.quantity);
-                totalCostAtPurchase -= parseFloat(elem.purchaseCost);
-            }
-        });
-        let currentTotalValueOfShares = 0;
-        stonkData.forEach((elem) => {
-            currentTotalValueOfShares += stonksOwned[elem.name] * elem.price;
-        });
-
-        return currentTotalValueOfShares - totalCostAtPurchase;
-    };
-
-    const getTotalShares = () => {
-        let stonksOwned = {
-            React: 0,
-            Angular: 0,
-            Vue: 0,
-        };
-        userStats.buysAndSells.forEach((elem) => {
-            if (elem.type === "BUY") {
-                stonksOwned[elem.stockName] += parseInt(elem.quantity);
-            } else {
-                stonksOwned[elem.stockName] -= parseInt(elem.quantity);
-            }
-        });
-        return stonksOwned;
-    };
-
-    const balance = getBalance();
-    const portfolioValue = getPortfolioValue();
-    const netWorth = getNetWorth();
-    const profitLoss = getProfitLoss();
-    const totalShares = getTotalShares();
+    const {
+        userStats,
+        userStatsDispatch,
+        balance,
+        portfolioValue,
+        netWorth,
+        profitLoss,
+        totalShares,
+    } = useContext(GuestUserContext);
     return (
         <>
             <Wrapper>
@@ -136,7 +38,7 @@ const App = () => {
                                     <CardContent stonkData={stonkData} />
                                 </Route>
                                 <Route exact path="/stonk/:stonkname">
-                                    <StonkContent
+                                    <Stonks
                                         stonkData={stonkData}
                                         userStats={userStats}
                                         userStatsDispatch={userStatsDispatch}
