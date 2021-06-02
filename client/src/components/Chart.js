519034573c7762d010fd3c69d4ca2d52188dce4a
Mustafa Ozturk <mozturkpro@gmail.com>
2021-06-02 16:19:42 -0400

    changed class component chart to functional

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,29 +1,25 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { LineChart, Line, Tooltip, ResponsiveContainer } from "recharts";
 
-export default class Example extends PureComponent {
-    render() {
-        return (
-            <ResponsiveContainer width="100%" height="85%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={this.props.priceHistory}
-                >
-                    <Tooltip
-                        separator=""
-                        cursor={false}
-                        labelFormatter={() => ""}
-                    />
-                    <Line
-                        type="linear"
-                        dataKey="$"
-                        stroke="rgb(14, 184, 239)"
-                        dot={false}
-                        strokeWidth={3}
-                    />
-                </LineChart>
-            </ResponsiveContainer>
-        );
-    }
-}
+const Chart = ({ priceHistory }) => {
+    return (
+        <ResponsiveContainer width="100%" height="85%">
+            <LineChart width={500} height={300} data={priceHistory}>
+                <Tooltip
+                    separator=""
+                    cursor={false}
+                    labelFormatter={() => ""}
+                />
+                <Line
+                    type="linear"
+                    dataKey="$"
+                    stroke="rgb(14, 184, 239)"
+                    dot={false}
+                    strokeWidth={3}
+                />
+            </LineChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default Chart;
