8b863341bbbe5199aa4b53ee30c0f66072a7da00
Mustafa Ozturk <mozturkpro@gmail.com>
2021-06-05 16:23:34 -0400

    add handler functions for server

--- server/handlers.js
+++ server/handlers.js
@@ -1,6 +1,10 @@
-const { stonkData, createUser } = require("./utils");
+const Axios = require("axios");
+const db = require("./db.json");
+const { stonkData } = require("./utils");
 
 const stonkDataArr = stonkData();
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 const handleTest = (req, res) => {
     try {
@@ -24,7 +28,50 @@ const handleCards = (req, res) => {
     }
 };
 
+const handleSigninRedirect = (req, res) => {
+    res.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+    );
+};
+
+const handleOauthCallback = async (req, res) => {
+    const code = req.query.code;
+    const { data } = await Axios.post(
+        `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`
+    );
+    const accessTokenParams = new URLSearchParams(data);
+
+    const accessToken = accessTokenParams.get("access_token");
+    // const refreshToken = accessTokenParams.get("refresh_token");
+
+    const { data: ghData } = await Axios.get("https://api.github.com/user", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    const user = {
+        id: ghData.id,
+        username: ghData.login,
+    };
+    db.push(user);
+    res.redirect(`http://localhost:3000?id=${ghData.id}`);
+};
+
+const handleUserAuth = (req, res) => {
+    const { id } = req.body;
+    db.find((elem) => {
+        if (elem.id === parseInt(id)) {
+            return res.json({ message: "user logged in" });
+        }
+        return res.json({ message: "user not found" });
+    });
+};
+
 module.exports = {
     handleTest,
     handleCards,
+    handleSigninRedirect,
+    handleOauthCallback,
+    handleUserAuth,
 };
--- server/server.js
+++ server/server.js
@@ -3,62 +3,29 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const Axios = require("axios");
-const db = require("./db.json");
 const app = express();
 
-const { handleTest, handleCards, handleGuessUser } = require("./handlers");
+const {
+    handleTest,
+    handleCards,
+    handleSigninRedirect,
+    handleOauthCallback,
+    handleUserAuth,
+} = require("./handlers");
 
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(cors());
 app.use(express.json());
-app.get("/test", handleTest);
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+app.get("/test", handleTest);
 
 app.get("/api/stonkData", handleCards);
 
-app.get("/api/user/signin", (req, res) => {
-    res.redirect(
-        `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
-    );
-});
+app.get("/api/user/signin", handleSigninRedirect);
 
-app.get("/oauth-callback", async (req, res) => {
-    const code = req.query.code;
-    const { data } = await Axios.post(
-        `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`
-    );
-    const accessTokenParams = new URLSearchParams(data);
+app.get("/oauth-callback", handleOauthCallback);
 
-    const accessToken = accessTokenParams.get("access_token");
-    // const refreshToken = accessTokenParams.get("refresh_token");
-
-    const { data: ghData } = await Axios.get("https://api.github.com/user", {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-
-    const user = {
-        id: ghData.id,
-        username: ghData.login,
-    };
-    db.push(user);
-
-    res.redirect(`http://localhost:3000?id=${ghData.id}`);
-});
-
-app.post("/api/user/auth", (req, res) => {
-    const { id } = req.body;
-    db.find((elem) => {
-        if (elem.id === parseInt(id)) {
-            return res.json({ message: "user logged in" });
-        }
-        return res.json({ message: "user not found" });
-    });
-});
+app.post("/api/user/auth", handleUserAuth);
 
 app.get("*", (req, res) => {
     res.status(404).json({
