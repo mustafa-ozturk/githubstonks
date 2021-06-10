301d5e80e78df77ac199358fb4e79238a107ebda
Mustafa Ozturk <mozturkpro@gmail.com>
2021-06-10 17:09:12 -0400

    get stars forks commits from github api but running into rate limit

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,6 @@
-// get this data from github
+const Axios = require("axios");
+const fetch = require("node-fetch");
+// I would get this data from github but rate limiting and stuff...
 const githubData = [
     {
         logo: "/assets/react.svg",
@@ -34,34 +36,85 @@ const githubData = [
         priceHistory: [{ "Price: $": 0 }],
     },
 ];
-// const initialSharePrice = (stockname) => {
-//     const result = githubData.find((e) => e.name === stockname);
-//     if (result) {
-//         return (
-//             result.stars * 0.0003 +
-//             result.forks * 0.0002 +
-//             result.commits * 0.0001
-//         );
+
+// thank you https://github.com/matthew-e-brown for helping with this (finding total commit count)
+// const BASE_URL = "https://api.github.com";
+
+// const myFetch = (url) =>
+//     fetch(url, {
+//         // github api requires a user-agent to avoid rate limit
+//         headers: { "user-agent": "commit-counter" },
+//     });
+
+// const getCurrentCommitSHA = async (branchURL) => {
+//     const branchData = await myFetch(branchURL).then((res) => res.json());
+//     return branchData.commit.sha;
+// };
+
+// const getFirstCommitSHA = async (repoURL) => {
+//     let response = await myFetch(`${repoURL}/commits`);
+
+//     const link = response.headers.get("link") || response.headers.get("Link");
+
+//     if (link) {
+//         const firstPage = /<([^>]+)>; rel="last"/.exec(link)[1];
+//         response = await myFetch(firstPage);
+//     }
+
+//     const firstCommit = (await response.json()).pop();
+//     return firstCommit.sha;
+// };
+
+// const getCommitCount = async (...args) => {
+//     let owner, repo;
+
+//     if (args.length == 2) {
+//         [owner, repo] = args;
+//     } else if (args.length == 1) {
+//         [owner, repo] = args[0].split("/");
+//     } else {
+//         throw new Error("Incorrect args, ('owner', 'repo') or ('owner/repo')");
+//     }
+
+//     const repoURL = `${BASE_URL}/repos/${owner}/${repo}`;
+
+//     // Get default branch and branch URL
+//     const branchURL = await myFetch(repoURL)
+//         .then((res) => res.json())
+//         .then(({ branches_url, default_branch }) => {
+//             return branches_url.replace("{/branch}", `/${default_branch}`);
+//         });
+
+//     // Check what commit it's at
+//     const currentSHA = await getCurrentCommitSHA(branchURL);
+
+//     // Check what the first commit was
+//     const firstSHA = await getFirstCommitSHA(repoURL);
+
+//     const comp = await myFetch(
+//         `${repoURL}/compare/${currentSHA}...${firstSHA}`
+//     ).then((res) => res.json());
+
+//     switch (comp.status) {
+//         case "behind":
+//             return comp.behind_by;
+//         case "ahead":
+//             return comp.ahead_by;
 //     }
 // };
 
-// const sharePriceAfterUserMarket = (stockname) => {
-//     const result = githubData.find((e) => e.name === stockname);
-//     if (result) {
-//         return initialSharePrice(stockname) + result.totalBoughtShares * 0.001;
-//     }
+// const ghStarsAndForks = async (link) => {
+//     const data = await fetch(`https://api.github.com/${link}`);
+//     const reactData = {
+//         stars: data.stargazers_count,
+//         forks: data.forks,
+//     };
+//     console.log(data);
+//     return reactData;
 // };
 
-// const dollarIncreaseFromInitialPrice = (stockname) => {
-//     return sharePriceAfterUserMarket(stockname) - initialSharePrice(stockname);
-// };
-
-// const percentIncreaseFromInitialPrice = (stockname) => {
-//     return (
-//         (100 * dollarIncreaseFromInitialPrice(stockname)) /
-//         initialSharePrice(stockname)
-//     );
-// };
+// getCommitCount("facebook/react").then((num) => console.log(num));
+// ghStarsAndForks("facebook/react");
 
 const stonkData = () => {
     const stonkDataArray = githubData.map((elem, index) => {
