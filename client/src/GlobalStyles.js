20ba2a608a0d263a8aeefa8bada7e3f97c103d7d
Mustafa Ozturk <mozturk@spiria.com>
2021-11-06 21:53:56 -0400

    a new beginning

--- client/src/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-export default createGlobalStyle`
-    :root {
-        --main-blue: rgb(14, 184, 239);
-        --main-red: rgb(221, 21, 33);
-        --main-boxshadow: 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.12),
-        0px 1px 3px rgba(0, 0, 0, 0.2);;
-    }
-    `;
--- server/ObjectOfTokens.js
+++
@@ -1,5 +0,0 @@
-const ObjectOfTokens = {};
-
-module.exports = {
-    ObjectOfTokens,
-};
